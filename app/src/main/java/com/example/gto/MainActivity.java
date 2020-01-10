549293bc5553f048e3370a615681f47dfe0fef87
zhiyan Yao <rachel2804195717@icloud.com>
2020-01-10 16:49:20 +0800

    Main GOTO create_trip added; create_trip GOTO choose_baggage added.

--- app/src/main/java/com/example/gto/MainActivity.java
+++ app/src/main/java/com/example/gto/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.gto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gotoCreate(View v) {
+        Intent it = new Intent(this, create_trip.class);
+        startActivity(it);
+    }
+
+    public void gotoEdit(View v) {
+        Intent it = new Intent(this, edit_trip.class);
+        startActivity(it);
     }
 }
--- app/src/main/java/com/example/gto/create_trip.java
+++ app/src/main/java/com/example/gto/create_trip.java
@@ -2,7 +2,9 @@ package com.example.gto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class create_trip extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class create_trip extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_trip);
+    }
+
+    public void gotoChoose_baggage(View v) {
+        Intent it = new Intent (this, choose_baggage.class);
+        startActivity(it);
     }
 }
