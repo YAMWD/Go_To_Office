3867051d9358721b8c2159ae5e526808b3b47680
zhiyan Yao <rachel2804195717@icloud.com>
2020-01-10 16:17:58 +0800

    four more activities added

---
+++ app/src/main/java/com/example/gto/edit_trip.java
@@ -0,0 +1,14 @@
+package com.example.gto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class edit_trip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_trip);
+    }
+}
